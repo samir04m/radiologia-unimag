e226847fc5355b48cd8714bcdbeca16775fd9c26
Samir Mejia <samir04m@gmail.com>
2019-11-17 17:53:08 -0500

    Mano 1, 2 y 4 lista

--- src/app/components/radiografias/superiores/mano/mano.component.ts
+++ src/app/components/radiografias/superiores/mano/mano.component.ts
@@ -20,4 +20,33 @@ export class ManoComponent implements OnInit {
      jQuery("#nombre").html(title);
   }
 
+  onClick2(elem){
+     let title = jQuery(elem.target).attr('title');
+     $('path').removeClass('selected');
+     $(elem.target).addClass('selected');
+     jQuery("#nombre2").html(title);
+  }
+
+  onClick3(elem){
+     let title = jQuery(elem.target).attr('title');
+     $('path').removeClass('selected');
+     $(elem.target).addClass('selected');
+     jQuery("#nombre3").html(title);
+  }
+
+  onClick4(elem){
+     let title = jQuery(elem.target).attr('title');
+     $('path').removeClass('selected');
+     $(elem.target).addClass('selected');
+     jQuery("#nombre4").html(title);
+  }
+
+  onClick5(elem){
+     let title = jQuery(elem.target).attr('title');
+     $('path').removeClass('selected');
+     $(elem.target).addClass('selected');
+     jQuery("#nombre5").html(title);
+  }
+
+
 }
