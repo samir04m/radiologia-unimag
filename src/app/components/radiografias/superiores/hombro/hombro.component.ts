93d5d370980368c87d8ffea5c9e6b96092caf270
Samir Mejia <samir04m@gmail.com>
2019-11-17 00:35:40 -0500

    Correccion de error

--- src/app/components/radiografias/superiores/hombro/hombro.component.ts
+++ src/app/components/radiografias/superiores/hombro/hombro.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import * as jQuery from 'jquery';
+
 
 @Component({
   selector: 'app-hombro',
   templateUrl: './hombro.component.html',
-  styleUrls: ['./hombro.component.css']
+  styleUrls: ['../../radiografias.component.css']
 })
 export class HombroComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-     // window.location.reload();
-  }
+   ngOnInit() {
 
+   }
+
+   onClick(elem){
+      // console.log("select", elem.target, elem.target.attributes.id);
+      let title = jQuery(elem.target).attr('title');
+      $('path').removeClass('selected');
+      $(elem.target).addClass('selected');
+      jQuery("#nombre").html(title);
+   }
 
 }
